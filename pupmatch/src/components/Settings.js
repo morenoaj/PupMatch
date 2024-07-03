795c350fea7bac9ed6d13d40b41d197376bb7de4
Alex Javier Moreno <63484452+morenoaj@users.noreply.github.com>
2024-07-03 01:00:13 -0500

    2: Agregando cambios v2 (AddPhotos)

---
+++ pupmatch/src/components/Settings.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Settings = () => {
+  return (
+    <div className="settings">
+      {/* Contenido del componente */}
+    </div>
+  );
+};
+
+export default Settings;
