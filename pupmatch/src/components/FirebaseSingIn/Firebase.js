a90ef2ccee4310f06f00240923f27aa4fbbf175c
ElRinconDeGabs <isaac.samaniego@utp.ac.pa>
2024-07-07 19:29:14 -0500

    ...

--- pupmatch/src/components/FirebaseSingIn/Firebase.js
+++ pupmatch/src/components/FirebaseSingIn/Firebase.js
@@ -4,14 +4,7 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC4mQmaG1dxy4T1d67-pTDbK69VQso-OFE",
-  authDomain: "pupmatch-5e118.firebaseapp.com",
-  databaseURL: "https://pupmatch-5e118-default-rtdb.firebaseio.com",
-  projectId: "pupmatch-5e118",
-  storageBucket: "pupmatch-5e118.appspot.com",
-  messagingSenderId: "747085021886",
-  appId: "1:747085021886:web:ec4244c66a3ce5d25dd4ef",
-  measurementId: "G-JSCWHJ7G0N",
+  
 };
 
 // Initialize Firebase
